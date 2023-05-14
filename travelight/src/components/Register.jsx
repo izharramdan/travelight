b2a3e18f4ad2d007f541f3cb891b4e752954b580
Izhar <izharramdamn@gmail.com>
2023-05-14 10:02:18 +0700

    edit

--- travelight/src/components/Register.jsx
+++ travelight/src/components/Register.jsx
@@ -1,34 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 
 function Register() {
   const [formData, setFormData] = useState({
-    email: '',
-    name: '',
-    password: '',
-    passwordRepeat: '',
-    role: '',
-    profilePictureUrl: '',
-    phoneNumber: '',
+    email: "",
+    name: "",
+    password: "",
+    passwordRepeat: "",
+    role: "",
+    profilePictureUrl: "",
+    phoneNumber: "",
   });
 
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post(
-      'https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/register',
-      formData,
-      {
-        headers: {
-          'apiKey': '24405e01-fbc1-45a5-9f5a-be13afcd757c',
-        },
-      },
-    )
-      .then(response => {
+    axios
+      .post(
+        "https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/register",
+        formData,
+        {
+          headers: {
+            apiKey: "24405e01-fbc1-45a5-9f5a-be13afcd757c",
+          },
+        }
+      )
+      .then((response) => {
         console.log(response.data);
+        alert("Registration successful!");
+        navigate('/');
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error.response.data);
         setError(error.response.data);
       });
@@ -45,26 +50,68 @@ function Register() {
   return (
     <form onSubmit={handleSubmit} className="register-form">
       <label htmlFor="email">Email:</label>
-      <input type="email" name="email" value={formData.email} onChange={handleChange} required />
-  
+      <input
+        type="email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        required
+      />
+
       <label htmlFor="name">Name:</label>
-      <input type="text" name="name" value={formData.name} onChange={handleChange} required />
-  
+      <input
+        type="text"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+        required
+      />
+
       <label htmlFor="password">Password:</label>
-      <input type="password" name="password" value={formData.password} onChange={handleChange} required />
-  
+      <input
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+        required
+      />
+
       <label htmlFor="passwordRepeat">Repeat Password:</label>
-      <input type="password" name="passwordRepeat" value={formData.passwordRepeat} onChange={handleChange} required />
-  
+      <input
+        type="password"
+        name="passwordRepeat"
+        value={formData.passwordRepeat}
+        onChange={handleChange}
+        required
+      />
+
       <label htmlFor="role">Role:</label>
-      <input type="text" name="role" value={formData.role} onChange={handleChange} required />
-  
+      <input
+        type="text"
+        name="role"
+        value={formData.role}
+        onChange={handleChange}
+        required
+      />
+
       <label htmlFor="profilePictureUrl">Profile Picture URL:</label>
-      <input type="url" name="profilePictureUrl" value={formData.profilePictureUrl} onChange={handleChange} required />
-  
+      <input
+        type="url"
+        name="profilePictureUrl"
+        value={formData.profilePictureUrl}
+        onChange={handleChange}
+        required
+      />
+
       <label htmlFor="phoneNumber">Phone Number:</label>
-      <input type="tel" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} required />
-  
+      <input
+        type="tel"
+        name="phoneNumber"
+        value={formData.phoneNumber}
+        onChange={handleChange}
+        required
+      />
+
       <button type="submit">Register</button>
     </form>
   );
